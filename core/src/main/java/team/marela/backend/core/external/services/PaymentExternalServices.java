9921cfb3f4ebca40357e70ebe086e9d1ba95d83c
JakMar17 <jakob.marusic17@gmail.com>
2023-01-11 19:04:04 +0100

    added metrics

--- core/src/main/java/team/marela/backend/core/external/services/PaymentExternalServices.java
+++ core/src/main/java/team/marela/backend/core/external/services/PaymentExternalServices.java
@@ -1,5 +1,8 @@
 package team.marela.backend.core.external.services;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
@@ -12,33 +15,62 @@ import team.marela.backend.core.external.models.payments.PaymentsExternalInvoice
 import team.marela.backend.database.entities.ParticipantEntity;
 import team.marela.backend.database.entities.entries.EntryEntity;
 
+import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentExternalServices {
     private final RestTemplate restTemplate = new RestTemplate();
     private final CircuitBreakerFactory circuitBreakerFactory;
+    private final MeterRegistry meterRegistry;
 
     @Value("${external.url.majskeigre_payments_url}")
     private String paymentsBaseUrl;
 
+    private Counter invoiceCreationSuccessCounter;
+    private Counter invoiceCreationFailureCounter;
+
+    @PostConstruct
+    public void postConstruct() {
+        invoiceCreationSuccessCounter = Counter
+                .builder("external__payments__invoiceCreation_success")
+                .description("Number of successfully created invoice creation requests")
+                .register(meterRegistry);
+        invoiceCreationFailureCounter = Counter
+                .builder("external__payments__invoiceCreation_failure")
+                .description("Number of failures in invoice creation requests")
+                .register(meterRegistry);
+    }
+
 
     @Async
+    @Timed(value = "external__payments__invoiceCreation_requestTime", description = "Time to process invoice creation", percentiles = {0.25, 0.95})
     public CompletableFuture<PaymentsExternalInvoiceDto> createInvoice(EntryEntity entry, ParticipantEntity participant) {
         return CompletableFuture.completedFuture(
                 circuitBreakerFactory.create("invoiceGeneratingCircuitBreaker").run(
-                       () ->  restTemplate.postForEntity(
-                               String.format("%s/invoice", paymentsBaseUrl),
-                               createInvoiceData(entry, participant),
-                               PaymentsExternalInvoiceDto.class
-                       ).getBody(),
-                       (throwable) -> null
-               )
+                        () -> createInvoiceCircuitBreakerRunnable(entry, participant),
+                        (throwable) -> createInvoiceCircuitBreakerFallback()
+                )
         );
+    }
+
+    private PaymentsExternalInvoiceDto createInvoiceCircuitBreakerRunnable(EntryEntity entry, ParticipantEntity participant) {
+        var res = restTemplate.postForEntity(
+                String.format("%s/invoice", paymentsBaseUrl),
+                createInvoiceData(entry, participant),
+                PaymentsExternalInvoiceDto.class
+        ).getBody();
+
+        invoiceCreationSuccessCounter.increment(1);
+        return res;
+    }
+
+    private PaymentsExternalInvoiceDto createInvoiceCircuitBreakerFallback() {
+        invoiceCreationFailureCounter.increment(1);
+        return null;
     }
 
     private PaymentsExternalInvoiceDto createInvoiceData(EntryEntity entry, ParticipantEntity participant) {
